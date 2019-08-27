b0886d5068a476b334e5c06be6c46a983c6d9d50
kiselevgleb <kiselevgleb@gmail.com>
2019-08-27 14:26:39 +0300

    update27.08.19-14.25

--- app/src/main/java/com/example/notesandreminding/App.java
+++ app/src/main/java/com/example/notesandreminding/App.java
@@ -8,7 +8,6 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
         noteRepository = new NotesRepositoryImp(this);
         settingsManager = new SettingsManagerImp(this);
     }
